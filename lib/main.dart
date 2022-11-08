9a4b9a7b6ad973c7e407aa93cdaaa778557ce054
BalqisAsySyifaLumbun <balqislumbun@gmail.com>
2022-11-08 08:21:26 +0700

    Tugas 7

---
+++ lib/main.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Program Counter',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Program Counter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _gengan = 'GENAP';
+  bool isVisible = false;
+
+  // void _genganChangeColor() {
+  //   setState(() {
+  //     // This call to setState tells the Flutter framework that something has
+  //     // changed in this State, which causes it to rerun the build method below
+  //     // so that the display can reflect the updated values. If we changed
+  //     // _counter without calling setState(), then the build method would not be
+  //     // called again, and so nothing would appear to happen.
+  //     if (_counter % 2 == 0) {
+  //       _gengan = 'GENAP';
+  //     } else {
+  //       _gengan = 'GANJIL';
+  //     }
+  //   });
+  // }
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+      isVisible = true;
+      if (_counter % 2 == 0) {
+        _gengan = 'GENAP';
+      } else {
+        _gengan = 'GANJIL';
+      }
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      if (_counter == 0) {
+        isVisible = false;
+      } else {
+        isVisible = true;
+        _counter--;
+        if (_counter % 2 == 0) {
+          _gengan = 'GENAP';
+          if (_counter == 0) {
+            isVisible = false;
+          }
+        } else {
+          _gengan = 'GANJIL';
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor() {
+      if (_counter % 2 == 0) {
+        return Colors.red;
+      } else {
+        return Colors.blue;
+      }
+    }
+
+    // Visibility getVisible() {
+    //   if (_counter < 1) {
+    //     return false;
+    //   } else {
+    //     return true;
+    //   }
+    // }
+
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            '$_gengan',
+            style: TextStyle(color: getColor()),
+            // style: TextStyle(color: Colors.blue),
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              if (isVisible)
+                FloatingActionButton(
+                  onPressed: _decrementCounter,
+                  child: const Icon(Icons.remove),
+                ),
+              FloatingActionButton(
+                onPressed: _incrementCounter,
+                child: const Icon(Icons.add),
+              ),
+              // tooltip: 'Increment',
+              // tooltip: 'Decrement',
+            ],
+          ),
+        ],
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
